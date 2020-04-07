ec9e7dcbdab51c38894ba95ceb2ba47804df0502
bugragoksu <bugragoksu2@gmail.com>
2020-04-07 03:12:23 +0300

    add firebaseuser repository

---
+++ lib/src/services/repository/firebase_user_repository.dart
@@ -0,0 +1,22 @@
+import 'package:eventapp/src/services/api/firebase_sign.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseUserRepository {
+  FirebaseSign _sign = FirebaseSign();
+
+  signIn(String email, String password) async {
+    return _sign.signIn(email, password);
+  }
+
+  Future<String> signUp(String email, String password) async {
+    return _sign.signUp(email, password);
+  }
+
+  Future<FirebaseUser> getCurrentUser() async {
+    return _sign.getCurrentUser();
+  }
+
+  Future<void> signOut() async {
+    return _sign.signOut();
+  }
+}
