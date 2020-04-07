4a4c648fdbf57623239846968a58c6ba14f61302
bugragoksu <bugragoksu2@gmail.com>
2020-04-07 23:43:21 +0300

    add notification repository

---
+++ lib/src/services/repository/notification_repository.dart
@@ -0,0 +1,9 @@
+import 'package:eventapp/src/model/notification.dart';
+import 'package:eventapp/src/services/api/firebase_api.dart';
+
+class NotificationRepository {
+  FirebaseAPI _api = FirebaseAPI();
+  Future<List<Notification>> getNotifications(String uid) async {
+    return await _api.getNotifications(uid);
+  }
+}
