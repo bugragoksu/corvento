e81e1a4904b3f858a1ecaf7c28e7ac7414d71ef7
bugragoksu <bugragoksu2@gmail.com>
2020-04-09 15:32:23 +0300

    add notif documentid prop in notif model

--- lib/src/model/notification.dart
+++ lib/src/model/notification.dart
@@ -1,15 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserNotification {
+  final String documentID;
   final String title;
   final String data;
   final DateTime date;
 
-  UserNotification({this.title, this.data, this.date});
+  UserNotification({this.documentID, this.title, this.data, this.date});
 
   factory UserNotification.fromFirestore(DocumentSnapshot snapshot) {
     Map data = snapshot.data;
     return UserNotification(
+        documentID: snapshot.documentID,
         title: data["title"] ?? 'Yeni Bildirim',
         data: data['data'] ?? '',
         date: data['date'].toDate() ?? DateTime.now());
