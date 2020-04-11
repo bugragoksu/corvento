1968cb897d68f4a65fb4785ef7dd67df3d1bb71c
bugragoksu <bugragoksu2@gmail.com>
2020-04-12 00:54:48 +0300

    refactor Category model for server

--- lib/src/model/category.dart
+++ lib/src/model/category.dart
@@ -1,12 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Category {
+  final int id;
   final String name;
+  final String icon;
 
-  Category({this.name});
+  Category({this.id, this.name, this.icon});
 
   factory Category.fromFirestore(DocumentSnapshot snapshot) {
     Map data = snapshot.data;
     return Category(name: data["name"] ?? '');
   }
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+      id: json["id"] == null ? null : json["id"],
+      name: json["name"] == null ? null : json["name"],
+      icon: json["icon"]);
+
+  Map<String, dynamic> toJson() => {
+        "id": id == null ? null : id,
+        "name": name == null ? null : name,
+        "icon": icon
+      };
 }
